171cdc3739f040dc8c4c0e2619b5b7903a6e1caa
Jamela <jonathanlinch7@gmail.com>
2021-11-30 16:28:56 -0300

    7° commit, data class Funcionario

---
+++ src/main/kotlin/one/digitalinovation/collections/Funcionario.kt
@@ -0,0 +1,13 @@
+package one.digitalinovation.collections
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+){
+    override fun toString(): String =
+        """
+        Nome =  $nome
+        Salário =   $salario
+    """.trimIndent()
+}
